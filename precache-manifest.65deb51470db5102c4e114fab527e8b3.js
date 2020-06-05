b920f44ba52a36759479e3aa071fa604b09ff862
loapits <gera59377@gmail.com>
2020-06-05 23:22:01 +0600

    Updates

---
+++ precache-manifest.65deb51470db5102c4e114fab527e8b3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d91d8ae644a6eda47d1cea66628c823",
+    "url": "/Textshifter/index.html"
+  },
+  {
+    "revision": "84f20c6fe01b7dd45904",
+    "url": "/Textshifter/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b6cb01ed564b20d876b0",
+    "url": "/Textshifter/static/css/main.4bc3de78.chunk.css"
+  },
+  {
+    "revision": "84f20c6fe01b7dd45904",
+    "url": "/Textshifter/static/js/2.9b6cbd68.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Textshifter/static/js/2.9b6cbd68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6cb01ed564b20d876b0",
+    "url": "/Textshifter/static/js/main.5ec09c85.chunk.js"
+  },
+  {
+    "revision": "fd9587d3580b2b2fe613",
+    "url": "/Textshifter/static/js/runtime-main.88db9dd9.js"
+  },
+  {
+    "revision": "2c09403c0f2be29af7b292c3959edc8a",
+    "url": "/Textshifter/static/media/clipboard.2c09403c.svg"
+  },
+  {
+    "revision": "e24c2f90462ceb343e4496dfee9ad577",
+    "url": "/Textshifter/static/media/clipboard_dark.e24c2f90.svg"
+  },
+  {
+    "revision": "d365cb2a370fa03abe6e4024b35f6411",
+    "url": "/Textshifter/static/media/direct-download.d365cb2a.svg"
+  },
+  {
+    "revision": "b221dab5d37ec95af148c945a7cd68fc",
+    "url": "/Textshifter/static/media/direct_download_dark.b221dab5.svg"
+  },
+  {
+    "revision": "c140a1311aac6e1e60427721f217c31c",
+    "url": "/Textshifter/static/media/logo.c140a131.svg"
+  },
+  {
+    "revision": "1e712110ecde077bd331ec5ea39a1e7f",
+    "url": "/Textshifter/static/media/moon.1e712110.svg"
+  },
+  {
+    "revision": "02f11d1f91bda442b86a5763de986296",
+    "url": "/Textshifter/static/media/sun.02f11d1f.svg"
+  }
+]);
