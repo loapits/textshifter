25c01d69e5bcee40a129db3b604e649e85485e44
loapits <gera59377@gmail.com>
2020-06-24 22:30:00 +0600

    access

--- src/components/buttons/Buttons.tsx
+++ src/components/buttons/Buttons.tsx
@@ -8,12 +8,12 @@ import { IButton } from '../../interfaces/buttons_interface'
  */
 export const Buttons: React.FC<IButton> = (props) => (
   <div className={`button ${style.buttons}`}>
-    <button onClick={props.textUpperCase}>Uppercase</button>
-    <button onClick={props.textLowerCase}>Lowercase</button>
-    <button onClick={props.firstTitle}>First title</button>
-    <button onClick={props.inverseRegister}>Register Inversion</button>
-    <button onClick={props.startAnUpperCase}>Start an upper case</button>
-    <button onClick={props.asSentence}>As in the sentence</button>
-    <button onClick={props.replaceText}>Eng ⇄ Ru</button>
+    <button type="button" onClick={props.textUpperCase} aria-label="Convert text to uppercase">Uppercase</button>
+    <button type="button" onClick={props.textLowerCase} aria-label="Convert text to lowercase">Lowercase</button>
+    <button type="button" onClick={props.firstTitle} aria-label="Convert first letter of text to uppercase">First title</button>
+    <button type="button" onClick={props.inverseRegister} aria-label="Inverting case of text">Register Inversion</button>
+    <button type="button" onClick={props.startAnUpperCase} aria-label="Convert first letter of every word to uppercase">Start an upper case</button>
+    <button type="button" onClick={props.asSentence} aria-label="Convert text how in the sentence">As in the sentence</button>
+    <button type="button" onClick={props.replaceText} aria-label="If text wrote on ">Eng ⇄ Ru</button>
   </div>
 )
