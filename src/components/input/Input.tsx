ce8ae213102b5b56a46d8adbb9b70711c3a8d7d3
loapits <gera59377@gmail.com>
2020-06-24 23:18:55 +0600

    12

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -16,11 +16,11 @@ import { IInput } from '../../interfaces/input_interface'
  */
 export const Input: React.FC<IInput> = ({copied, editedText, enterText, copyToClipboard, enteredText, saveFileText}) => (
   <main id="main" className={style.textarea}>
-    <label>
+    <label>hi
       <textarea value={enterText || ''} onChange={enteredText} placeholder="Enter your text"></textarea>
     </label>
     <div className={style.resultInput}>
-      <label>
+      <label>hi
         <textarea value={editedText} readOnly placeholder="Your result"></textarea>
       </label>
       <CopyToClipboard onCopy={copyToClipboard} text={editedText}>
